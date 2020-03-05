6634604a4e41ff4f15280f2c64d18dbf54baeffe
Student <example@example.com>
2020-03-04 16:38:08 -0800

    update program to be a two player game that utilizes user inputs to correctly score and declare a winner. Play any number of best out of # rounds

--- RockPaperScissors/Models/Game.cs
+++ RockPaperScissors/Models/Game.cs
@@ -4,55 +4,82 @@ namespace RockPaperScissors.Models
 {
   public class Game
   {
-    public static int PlayerOneScore { get; set; }
-    public static int PlayerTwoScore { get; set; }
+    public static int PlayerOneScore { get; set; } = 0;
+    public static int PlayerTwoScore { get; set; } = 0;
 
     public string ChoiceOne { get; set; }
     public string ChoiceTwo { get; set; }
 
-    public Game(string choiceOne, string choiceTwo)
+    public static int InitialRounds { get; set; }
+
+    public Game(string choiceOne, string choiceTwo, int rounds)
     {
       ChoiceOne = choiceOne;
       ChoiceTwo = choiceTwo;
+      InitialRounds = rounds;
     }
 
     public void Battle()
     {
-      bool PlayerOneWins;
+      bool PlayerOneWins = false;
       if (SameHandEvent(ChoiceOne, ChoiceTwo))
       {
-        break;
-      }
-      else if (ChoiceOne == "rock")
-      {
-        PlayerOneWins = RockBeats(ChoiceTwo);
-      }
-      else if (ChoiceOne == "paper")
-      {
-        PlayerOneWins = PaperBeats(ChoiceTwo);
+        Console.WriteLine("No winner. Play again!");
       }
       else
       {
-        PlayerOneWins = ScissorsBeats(ChoiceTwo);
-      }
+        if (ChoiceOne == "rock")
+        {
+          PlayerOneWins = RockBeats(ChoiceTwo);
+        }
+        else if (ChoiceOne == "paper")
+        {
+          PlayerOneWins = PaperBeats(ChoiceTwo);
+        }
+        else
+        {
+          PlayerOneWins = ScissorsBeats(ChoiceTwo);
+        }
 
-      if (PlayerOneWins)
-      {
-        PlayerOneScore++;
-      }
-      else
-      {
-        PlayerTwoScore++;
+        if (PlayerOneWins)
+        {
+          PlayerOneScore++;
+        }
+        else
+        {
+          PlayerTwoScore++;
+        }
       }
     }
 
-    public static bool SameHandEvent(string choicestevens, string kristasChoice)
+    public bool PrintScoreBoard()
     {
-      if (kristasChoice == stevensChoice)
+      Console.WriteLine($"Player One: {PlayerOneScore}");
+      Console.WriteLine($"Player Two: {PlayerTwoScore}");
+      Console.WriteLine("{check win}");
+      if (PlayerOneScore >= (InitialRounds + 1) / 2)
+      {
+        Console.WriteLine($"Game over! Player One wins!");
+        return false;
+      }
+      else if (PlayerTwoScore >= (InitialRounds + 1) / 2)
+      {
+        Console.WriteLine($"Game over! Player Two wins!");
+        return false;
+      }
+      else
+      {
+        Console.WriteLine("Next round!");
+        return true;
+      }
+    }
+    public static bool SameHandEvent(string choice1, string choice2)
+    {
+      if (choice1 == choice2)
       {
         return true;
       }
-
+      return false;
     }
     public static bool RockBeats(string choice)
     {
@@ -88,61 +115,62 @@ namespace RockPaperScissors.Models
       }
     }
 
-    public static string CompareHands(string player1Choice, string player2Choice)
-    {
-      if (player1Choice == player2Choice)
-      {
-        return "tie";
-      }
-      else if (player1Choice == "rock")
-      {
-        if (player2Choice == "scissors")
-        {
-          return "rock";
-        }
-        else if (player2Choice == "paper")
-        {
-          return "paper";
-        }
-        else
-        {
-          return "Break";
-        }
-      }
-      else if (player1Choice == "paper")
-      {
-        if (player2Choice == "scissors")
-        {
-          return "scissors";
-        }
-        else if (player2Choice == "rock")
-        {
-          return "paper";
-        }
-        else
-        {
-          return "Break";
-        }
-      }
-      else if (player1Choice == "scissors")
-      {
-        if (player2Choice == "rock")
-        {
-          return "rock";
-        }
-        else if (player2Choice == "paper")
-        {
-          return "scissors";
-        }
-        else
-        {
-          return "Break";
-        }
-      }
-      else
-      {
-        return "get out of my house krista";
-      }
-    }
+    //   public static string CompareHands(string player1Choice, string player2Choice)
+    //   {
+    //     if (player1Choice == player2Choice)
+    //     {
+    //       return "tie";
+    //     }
+    //     else if (player1Choice == "rock")
+    //     {
+    //       if (player2Choice == "scissors")
+    //       {
+    //         return "rock";
+    //       }
+    //       else if (player2Choice == "paper")
+    //       {
+    //         return "paper";
+    //       }
+    //       else
+    //       {
+    //         return "Break";
+    //       }
+    //     }
+    //     else if (player1Choice == "paper")
+    //     {
+    //       if (player2Choice == "scissors")
+    //       {
+    //         return "scissors";
+    //       }
+    //       else if (player2Choice == "rock")
+    //       {
+    //         return "paper";
+    //       }
+    //       else
+    //       {
+    //         return "Break";
+    //       }
+    //     }
+    //     else if (player1Choice == "scissors")
+    //     {
+    //       if (player2Choice == "rock")
+    //       {
+    //         return "rock";
+    //       }
+    //       else if (player2Choice == "paper")
+    //       {
+    //         return "scissors";
+    //       }
+    //       else
+    //       {
+    //         return "Break";
+    //       }
+    //     }
+    //     else
+    //     {
+    //       return "get out of my house krista";
+    //     }
+    //   }
+    // }
   }
 }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -5,15 +5,37 @@ namespace RockPaperScissors
 {
   public class Program
   {
+    public static int rounds { get; set; }
     public static void Main()
     {
-      string stevensChoice = "rock";
-      string kristasChoice = "paper";
-      if (stevensChoice == "rock")
-      {
-        stevenWins = Game.RockBeats(kristasChoice);
-      }
+      Console.WriteLine("Play best out of ___. [Type an odd number]");
+      //steven wants a try catch
+      rounds = int.Parse(Console.ReadLine());
+      Console.WriteLine(rounds);
+      newRound();
+    }
 
+    public static void newRound()
+    {
+      Console.WriteLine("Player One: Type 'rock', 'paper', or 'scissors' to beat your opponent!");
+      string playerOneChoice = Console.ReadLine();
+      Console.WriteLine("Player Two: Type 'rock', 'paper', or 'scissors' to beat your opponent!");
+      string playerTwoChoice = Console.ReadLine();
+      Game game = new Game(playerOneChoice, playerTwoChoice, rounds);
+      game.Battle();
+      if (game.PrintScoreBoard())
+      {
+        newRound();
+      }
+      else
+      {
+        Console.WriteLine("Play again? [y/n]");
+        string choice = Console.ReadLine().ToLower();
+        if (choice == "y")
+        {
+          Main();
+        }
+      }
     }
   }
 }
